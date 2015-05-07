013846ed850ddcbf17a4c5f830e467320772f86c
jgn-epp <jgn.epp@gmail.com>
2015-05-07 12:39:02 +0200

    Removed sqlite jdbc driver from classpath

--- 04-Implemetation/FFS/src/dataLayer/CustomerDataAccess.java
+++ 04-Implemetation/FFS/src/dataLayer/CustomerDataAccess.java
@@ -1,53 +1,40 @@
 package dataLayer;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import domainLayer.Customer;
 
 public class CustomerDataAccess {
-  
-  private static final String SELECT = "SELECT * FROM Customer";
-
-  public Customer readCustomer() throws SQLException {
-    
-    Connection connection = null;
-    PreparedStatement statement = null;
-    ResultSet resultset = null;
-    Customer customer = null;
-    
-    try {
-      connection = new Connect().getConnection();
-    } finally {
-      if (connection != null) {
-        connection.close();
-      }
-    }
-    
-    try {
-      statement = connection.prepareStatement(SELECT);
-      resultset = statement.executeQuery();
-      customer = new Customer();
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
       
-      while (resultset.next()) {
-        customer.setId(resultset.getInt("CustomerID"));
-        customer.setFirstName(resultset.getString("firstName"));
-        customer.setLastName(resultset.getString("lastName"));
-        customer.setBadStanding(resultset.getInt("badStanding"));
+      Connection connection = null;
+      PreparedStatement statement = null;
+      ResultSet resultset = null;
+      Customer customer = null;
+      
+      try {
+        connection = new Connect().getConnection();
+      } 
+      finally {
+        if (connection != null)
+          connection.close();
       }
       
-      return customer;
-    
-    } finally {
-      if (resultset != null) {
-        resultset.close();
-      }
-      
-      if (statement != null) {
-        statement.close();
-      }
-    }
+      try {
+        statement = connection.prepareStatement("SELECT * FROM customer");
+        resultset = statement.executeQuery();
+        customer = new Customer();
+        
+        while (resultset.next()) {
+          customer = new Customer();
+          customer.setAlbumTitel(resultset.getString("albumtitel"));
+          customer.setUdgivelse(resultset.getString("udgivelse"));
+          customer.setAid(resultset.getInt("aid"));
+          list.add(customer);
+        }     
+        return list;
   }
-  
 }
