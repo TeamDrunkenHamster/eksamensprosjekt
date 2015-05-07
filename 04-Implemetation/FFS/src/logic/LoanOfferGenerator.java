1376d176b94220072c9b6bbc9eef3de30591efe7
Tom Engelsen <uninet@live.no>
2015-05-07 12:13:53 +0200

    added temp method for customer

--- 04-Implemetation/FFS/src/logic/LoanOfferGenerator.java
+++ 04-Implemetation/FFS/src/logic/LoanOfferGenerator.java
@@ -1,8 +1,11 @@
 package logic;
 
+import domainLayer.Customer;
 import domainLayer.LoanOffer;
 
 public interface LoanOfferGenerator {
 	
 	public void createLoanOffer(LoanOffer loanOffer);
+	
+	public void createCustomer(Customer customer);
 }
