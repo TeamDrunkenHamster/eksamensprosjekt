163ac770fd73647f1d227fff89759b1f53b639d1
jgn-epp <jgn.epp@gmail.com>
2015-05-11 10:03:54 +0200

    DatabaseSetupImpl tested
    it works! Remaining table creation needs to be added to the
    createTables() method

--- 04-Implemetation/FFS/src/dataLayer/DatabaseSetupImpl.java
+++ 04-Implemetation/FFS/src/dataLayer/DatabaseSetupImpl.java
@@ -5,7 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseSetupImpl implements DatabaseSetup {
-  //jeg ved ikke endnu om det virker!!!!!!!!!!
+ 
+  //Here will be added the creation of the remaining tables.
   private static final String CREATE_CUSTOMER = "CREATE TABLE IF NOT EXISTS Customer(customerID INTEGER PRIMARY KEY, firstName varchar(255), lastName varchar(255), badStanding INTEGER)";
   
   public void createTables(Connection connection) throws SQLException {
