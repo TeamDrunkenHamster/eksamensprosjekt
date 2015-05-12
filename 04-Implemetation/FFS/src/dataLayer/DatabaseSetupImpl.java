94c151c00f832d5d0724e6cbc31a3fbe6070a286
DMU14-Martin <M.skjoedt@live.dk>
2015-05-12 13:38:16 +0200

    updated Datamodel, updated DatabaseSetupImpl
    added prepared statements : FOREIGN_KEY_ON, CREATE_CAR_TABLE and
    CREATE_LOANOFFER_TABLE

--- 04-Implemetation/FFS/src/dataLayer/DatabaseSetupImpl.java
+++ 04-Implemetation/FFS/src/dataLayer/DatabaseSetupImpl.java
@@ -7,14 +7,19 @@ import java.sql.SQLException;
 public class DatabaseSetupImpl implements DatabaseSetup {
  
   //Here will be added the creation of the remaining tables.
+  private static final String FOREIGN_KEYS_ON = "PRAGMA foreign_keys = ON";
   private static final String CREATE_CUSTOMER_TABLE = "CREATE TABLE IF NOT EXISTS Customer(customerID INTEGER PRIMARY KEY, firstName VARCHAR(255), lastName VARCHAR(255), badStanding INTEGER)";
   private static final String CREATE_SALESMAN_TABLE = "CREATE TABLE IF NOT EXISTS Salesman(salesmanID INTEGER PRIMARY KEY, firstName VARCHAR(255), lastName VARCHAR(255), loanValueLimit INTEGER)";
-  
+  private static final String CREATE_CAR_TABLE = "CREATE TABLE IF NOT EXISTS Car(carID INTEGER PRIMARY KEY, model VARCHAR(255), price DOUBLE)";
+  private static final String CREATE_LOANOFFER_TABLE = "CREATE TABLE IF NOT EXISTS LoanOffer(loanID INTEGER PRIMARY KEY, customerID INTEGER, salesmanID INTEGER, carID INTEGER, totalInterestRate DOUBLE, downPayment DOUBLE, loanSize DOUBLE, paymentPeriodInMonths INTEGER, startDate VARCHAR, aprovedStatus BOOLEAN, FOREIGN KEY(customerID) REFERENCES Customer(customerID), FOREIGN KEY(salesmanID) REFERENCES Salesman(salesmanID), FOREIGN KEY(carID) REFERENCES Car(carID))";
+
   public void createDatabase(Connection connection) throws SQLException {
     
+	createTable(connection, FOREIGN_KEYS_ON);
     createTable(connection, CREATE_CUSTOMER_TABLE);
     createTable(connection, CREATE_SALESMAN_TABLE);
-    
+    createTable(connection, CREATE_CAR_TABLE);
+    createTable(connection, CREATE_LOANOFFER_TABLE);
   }
   
   private void createTable(Connection connection, String sql) throws SQLException {
