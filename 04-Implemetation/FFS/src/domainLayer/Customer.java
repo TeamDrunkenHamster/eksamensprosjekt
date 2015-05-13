bdd375290318c4c33aac87078035152d33561faa
Tom <uninet@live.no>
2015-05-13 09:18:37 +0200

    Added cprNumber to Customer

--- 04-Implemetation/FFS/src/domainLayer/Customer.java
+++ 04-Implemetation/FFS/src/domainLayer/Customer.java
@@ -1,32 +1,37 @@
 package domainLayer;
 
 public class Customer extends Person {
-  private String firstName;
-  private String lastName;
-  private int badStanding;
-  
-  public String getFirstName() {
-    return firstName;
-  }
-  public void setFirstName( String firstName ) {
-    this.firstName = firstName;
-  }
-  public String getLastName() {
-    return lastName;
-  }
-  public void setLastName( String lastName ) {
-    this.lastName = lastName;
-  }
-  public int getBadStanding() {
-    return badStanding;
-  }
-  public void setBadStanding( int badStanding ) {
-    this.badStanding = badStanding;
-  }
-  @Override
-  public String toString() {
-    return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", badStanding=" + badStanding + ", toString()=" + super.toString() + "]";
-  }
+	private String firstName;
+	private String lastName;
+	private String CPR;
+	private boolean badStanding;
 
-  
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName( String firstName ) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName( String lastName ) {
+		this.lastName = lastName;
+	}
+	public String getCPR() {
+		return CPR;
+	}
+	public void setCPR(String cPR) {
+		CPR = cPR;
+	}
+	public boolean getBadStanding() {
+		return badStanding;
+	}
+	public void setBadStanding( boolean badStanding ) {
+		this.badStanding = badStanding;
+	}
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", badStanding=" + badStanding + ", toString()=" + super.toString() + "]";
+	}
 }
