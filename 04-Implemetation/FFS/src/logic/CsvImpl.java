e1b79e00deaae529f6895f89130e875167b6b224
Tom <uninet@live.no>
2015-05-19 07:30:08 +0200

    Changes to CSV export
    Reorganized order of information

--- 04-Implemetation/FFS/src/logic/CsvImpl.java
+++ 04-Implemetation/FFS/src/logic/CsvImpl.java
@@ -1,23 +1,30 @@
 package logic;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import domainLayer.LoanOffer;
 
 public class CsvImpl implements Csv {
 	
-	public static final String DELIMITER = ",";
-	public static final String NEWLINE = "\n";
+	private static final String DELIMITER = ",";
+	private static final String NEWLINE = "\n";
+	private static final String HEADERS = "LoanID,CPRNumber,CustomerFirstName,CustomerLastName,"
+			+ "SalesmanID,CarModel,Downpayment,TotalInterestRate,APR,PaymentInMonths,StartDate,"
+			+ "ApprovalStatus,RejectedStatus";
 
 	@Override
 	public void exportToCSV(LoanOffer loanOffer, String path) {
 
 		try {
-			FileWriter writer = new FileWriter(path);
-			writer.write(formattedCustomer(loanOffer));
-			writer.close();
+			BufferedWriter buffer = new BufferedWriter(new FileWriter(path));
+			buffer.write(HEADERS);
+			buffer.newLine();
+			buffer.write(formattedCustomer(loanOffer));
+			buffer.close();
 		} catch (IOException e) {
-			// logger.log(e.getMessage) ?
+			System.out.println("crap");
 		}
 	}
 	
@@ -27,27 +34,31 @@ public class CsvImpl implements Csv {
 		
 		csvString.append(loanOffer.getLoanID());
 		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getLoanSize());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getTotalInterestRate());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getDownPayment());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getPaymentInMonths());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getApr());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getSalesman().getId());
-		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getCustomer().getId());
+		csvString.append(loanOffer.getCustomer().getCPR());
 		csvString.append(DELIMITER);
 		csvString.append(loanOffer.getCustomer().getFirstName());
 		csvString.append(DELIMITER);
 		csvString.append(loanOffer.getCustomer().getLastName());
 		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getCar().getModel());
+		csvString.append(loanOffer.getSalesman().getId());
 		csvString.append(DELIMITER);
-		csvString.append(loanOffer.getCar().getPrice());
+		csvString.append(loanOffer.getSalesman().getId());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getLoanSize());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getDownPayment());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getTotalInterestRate());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getApr());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getPaymentInMonths());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getStartDate());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getApprovedStatus());
+		csvString.append(DELIMITER);
+		csvString.append(loanOffer.getRejected());
 		csvString.append(NEWLINE);
 		
 		return csvString.toString();
