60149949ecc52917983d2928752695ba018e0bbc
DMU14-Martin <M.skjoedt@live.dk>
2015-05-20 09:59:27 +0200

    Added a readAllSalesman to SalesmanDAO and Impl

--- 04-Implemetation/FFS/src/dataLayer/SalesmanDAOImpl.java
+++ 04-Implemetation/FFS/src/dataLayer/SalesmanDAOImpl.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import domainLayer.Salesman;
 
 public class SalesmanDAOImpl implements SalesmanDAO {
   
+  private static final String SELECT_ALL = "SELECT * FROM Salesman";
   private static final String SELECT_FROM_ID = "SELECT salesmanID, loanValueLimit FROM Salesman WHERE salesmanID = ?";
   private static final String CREATE = "INSERT INTO Salesman (loanValueLimit) VALUES (?)";
   
@@ -53,5 +56,32 @@ public class SalesmanDAOImpl implements SalesmanDAO {
         statement.close();
     }
   }
+
+@Override
+public List<Salesman> readAllSalesman(Connection connection) throws SQLException {
+	
+	PreparedStatement statement = null;
+    ResultSet resultSet = null;
+    List<Salesman> salesmanList = null;
+	
+    try {
+        statement = connection.prepareStatement( SELECT_ALL );
+        resultSet = statement.executeQuery();
+        salesmanList = new ArrayList<Salesman>();
+        Salesman salesman = new Salesman();
+        
+        while(resultSet.next()) {
+          salesman.setSalesmanID((resultSet.getInt("salesmanID")));
+          salesman.setLoanValueLimit(resultSet.getInt("loanValueLimit"));
+          salesmanList.add(salesman);
+        }
+      } finally {
+        if (statement != null)
+          statement.close();
+        if (resultSet != null)
+          resultSet.close();
+      }
+      return salesmanList;
+}
 
 }
